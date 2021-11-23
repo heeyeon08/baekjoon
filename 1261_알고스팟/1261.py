c755b1364e13da71e1f7dee2c0e896431c876cc6
heeyeonkwon <76425337+heeyeon08@users.noreply.github.com>
2021-11-24 00:26:05 +0900

    Update 1261.py

--- 1261_알고스팟/1261.py
+++ 1261_알고스팟/1261.py
@@ -17,11 +17,11 @@ while q:
     ny = y + dy[i]
     if 0 <= nx < n and 0 <= ny < m:
       if dist[nx][ny] == -1:
-        if maze[nx][ny] == 0: # (nx, ny)위치의 노드가 0이면
+        if maze[nx][ny] == 0:       # (nx, ny)위치의 노드가 0이면
           dist[nx][ny] = dist[x][y] # 이전 가중치 그대로 가져옴
-          q.appendleft([nx,ny]) # 큐의 앞부분에 추가
+          q.appendleft([nx,ny])     # 큐의 앞부분에 추가
         else: # 1이면
           dist[nx][ny] = dist[x][y] + 1 # 이전 가중치에서 1 증가
-          q.append([nx,ny]) # 큐의 마지막에 추가
+          q.append([nx,ny])             # 큐의 마지막에 추가
 
 print(dist[n-1][m-1])
